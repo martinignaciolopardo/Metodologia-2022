11eb96fa0759d25b85f8fca60aa7b16ea9edc62b
Martin Lopardo <tincho921007cs@gmail.com>
2022-05-22 19:54:30 -0300

    se agrega routeAvanzado.php y RouterClass.php

---
+++ RouteAvanzado.php
@@ -0,0 +1,20 @@
+<?php
+    //require_once 'Controller/adminController.php';
+    //require_once 'Controller/publicController.php';
+    //require_once 'Controller/loginController.php';
+    //require_once 'RouterClass.php';
+    
+    // CONSTANTES PARA RUTEO
+    //define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    //define("LOGIN", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/login');
+    //define("LOGOUT", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/logout');
+
+    $r = new Router();
+
+    // PUBLIC CONTROLLER
+
+    //Ruta por defecto.
+    //$r->setDefaultRoute("publicController", "showHome");
+
+    //run
+    $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']); 
