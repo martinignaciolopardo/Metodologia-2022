d9bf318bf30a2921f267104f1203312ab0ba7828
Martin Lopardo <tincho921007cs@gmail.com>
2022-05-22 20:06:18 -0300

    Se agrega ruta por defecto en RouteAvanzado.php

--- RouteAvanzado.php
+++ RouteAvanzado.php
@@ -5,7 +5,7 @@
     //require_once 'RouterClass.php';
     
     // CONSTANTES PARA RUTEO
-    //define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
     //define("LOGIN", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/login');
     //define("LOGOUT", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/logout');
 
