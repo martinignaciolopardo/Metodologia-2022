f1714039dea75b1e99f4d072acd5c3c68e76a1fa
galeguay <galeguay@gmail.com>
2022-05-29 19:52:14 -0300

    Se corrigió el codigo de consulta filtrando por dias

--- Model/TurnsModel.php
+++ Model/TurnsModel.php
@@ -3,16 +3,7 @@
 class TurnsModel{
 
 function getTurnsByDay($idMedico, $fechaMin, $fechaMax = null){
-    if(!$fechaMax){
-    $sentencia = $this->db->prepare(
-        "SELECT * 
-        FROM turno
-        WHERE fecha >= CAST(? AS datetime)
-        AND fecha <= CAST(? AS datetime)
-        AND id_medico = ?
-    ORDER BY fecha;");
-    $sentencia->execute(array($fechaMin, $fechaMax, $idMedico));
-    }else{
+    if($fechaMax){
         $sentencia = $this->db->prepare(
             "SELECT * 
             FROM turno
@@ -21,8 +12,16 @@ function getTurnsByDay($idMedico, $fechaMin, $fechaMax = null){
             AND id_medico = ?
         ORDER BY fecha;");
         $sentencia->execute(array($fechaMin, $fechaMax, $idMedico));
+    }else{
+        $sentencia = $this->db->prepare(
+            "SELECT * 
+            FROM turno
+            WHERE fecha >= CAST(? AS datetime)
+            AND id_medico = ?
+        ORDER BY fecha;");
+        $sentencia->execute(array($fechaMin, $idMedico));
     }
-    return $sentencia->fetch(PDO::FETCH_OBJ);
+    return $sentencia->fetchAll(PDO::FETCH_OBJ);
 }
 
 }
