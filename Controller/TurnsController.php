a5b6719de08d8273edad884b560709f0bb7d5a57
galeguay <galeguay@gmail.com>
2022-05-29 20:09:52 -0300

    Se hicieron correciones en codigo para actualizar turnos segun filtro dias

--- Controller/TurnsController.php
+++ Controller/TurnsController.php
@@ -5,19 +5,21 @@ class TurnsController{
 //AGREGAR A FUNCION GET TURNOS
 /*
 
+    //ACA SE DEFINE ID_MEDICO
+    $idMedico = 1; //$_SESSION['id_medico'];
 
     $turnos = "";
     $fechaMin = null;
     $fechaMax = null;
 
     if (isset($_GET["fecha_min"]) && !empty($_GET["fecha_min"]))
-        $fechaMin = $_GET["fecha_min"];
+        $fechaMin = $_GET["fecha_min"];     //ver si hay que convertirlo a algun tipo de string o datetime
     if (isset($_GET["fecha_max"]) && !empty($_GET["fecha_max"]))
-        $fechaMax = $_GET["fecha_max"];
+        $fechaMax = $_GET["fecha_max"];     //ver si hay que convertirlo a algun tipo de string o datetime
 
     if($fechaMin != null && $fechaMax != null){
         if($fechaMax >= $fechaMin) //SI TIENE SETEADA AMBOS LIMITES & FECHAMAX ES MAYOR QUE EL FECHAMIN
-            $turnos = $this->model->getTurnsByDay($idMedico, $fechaMin, $fechaMax);
+            $turnos = $this->model->getTurnsByDay($idMedico, $fechaMin, $fechaMax); 
         else
             $turnos = $this->model->getTurnsByDay($idMedico, $fechaMin); //SE FECHAMAX > FECHMIN
     }else{
