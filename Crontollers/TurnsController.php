b00e291dc1efdaf19eb1c5c96dd75f23ea42837d
Martin Lopardo <tincho921007cs@gmail.com>
2022-05-30 17:02:23 -0300

    Agrego funcion showTurns al controlador, agrego al router la ruta al controler para mostrar los turnos

--- Crontollers/TurnsController.php
+++ Crontollers/TurnsController.php
@@ -5,7 +5,7 @@ require_once "./Metodologia-2022-vueTurns_TPE2-39/Views/TurnsView.php";
 require_once "./Metodologia-2022-vueTurns_TPE2-39/Models/TurnsModel.php";
 
 
-    class LoginController{
+    class TurnsController{
 
         private $model;
         private $view;
@@ -15,6 +15,22 @@ require_once "./Metodologia-2022-vueTurns_TPE2-39/Models/TurnsModel.php";
         $this->model = new TurnsModel();
         $this->view = new TurnsView();
         $this->authHelper = new AuthHelper();
-    }  
+    }
+
+    function showTurns(){
+        $this->authHelper->checkLoggedIn();
+        $this->authHelper->checkMedico();
+        $id = $_SESSION['id']
+        if($_GET['timeRange'] && isset($_GET['timeRange'])){
+            if ($_GET['timeRange'] == 'maniana') {
+                $action = $turn = $this->TurnsModel->showMorningTurns(1);  // verificar id medico
+            }if($_GET['timeRange'] == 'tarde'){
+                $action = $turn = $this->TurnsModel->getAfternoonTurns(1);  // verificar id medico
+            }else{
+                $action = $turn = $this->TurnsModel->showTurns; //mostrar todos los turnos (joel)
+            }
+        }
+        $this->view->renderTurns($action);
+    }
 
 }
