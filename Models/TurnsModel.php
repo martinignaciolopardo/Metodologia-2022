608e8d4cd34dbc459554505c54b5b01aabf85bcc
galeguay <galeguay@gmail.com>
2022-05-30 17:32:58 -0300

    Se combino la funcion de actualizar turnos

--- Models/TurnsModel.php
+++ Models/TurnsModel.php
@@ -54,4 +54,52 @@ class TurnsModel{
         }
         return $sentencia->fetchAll(PDO::FETCH_OBJ);
     }
+
+    function getTurnsByMorningAndDays($idMedico, $fechaMin, $fechaMax = null){
+        if($fechaMax == null){
+            $sentencia = $this->db->prepare(
+                "SELECT * 
+                FROM turno
+                WHERE fecha >= CAST(? AS datetime)
+                AND id_medico = ?
+                AND hour(fecha) BETWEEN '07:30:00' AND '11:30:00'
+                ORDER BY fecha;");
+            $sentencia->execute(array($fechaMin, $idMedico));
+        }else{
+            $sentencia = $this->db->prepare(
+                "SELECT * 
+                FROM turno
+                WHERE fecha >= CAST(? AS datetime)
+                AND fecha <= CAST(? AS datetime)
+                AND hour(fecha) BETWEEN '07:30:00' AND '11:30:00'
+                AND id_medico = ?
+            ORDER BY fecha;");
+            $sentencia->execute(array($fechaMin, $fechaMax, $idMedico));
+        }
+        return $sentencia->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function getTurnsByAfternoonAndDays($idMedico, $fechaMin, $fechaMax = null){
+        if($fechaMax == null){
+            $sentencia = $this->db->prepare(
+                "SELECT * 
+                FROM turno
+                WHERE fecha >= CAST(? AS datetime)
+                AND hour(fecha) BETWEEN '12:30:00' AND '16:30:00'.
+                AND id_medico = ?
+                ORDER BY fecha;");
+            $sentencia->execute(array($fechaMin, $idMedico));
+        }else{
+            $sentencia = $this->db->prepare(
+                "SELECT * 
+                FROM turno
+                WHERE fecha >= CAST(? AS datetime)
+                AND fecha <= CAST(? AS datetime)
+                AND hour(fecha) BETWEEN '12:30:00' AND '16:30:00'.
+                AND id_medico = ?
+            ORDER BY fecha;");
+            $sentencia->execute(array($fechaMin, $fechaMax, $idMedico));
+        }
+        return $sentencia->fetchAll(PDO::FETCH_OBJ);
+    }
 }
